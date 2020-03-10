d22fa8f110554a4b8c021227a5117f2cf8f512f0
jubayer047 <akjubayer047@gmail.com>
2020-03-10 23:49:55 +0600

    Intro Of JSX & Dynamic Style

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var mySelf = {
+    name:'Jubayer Alam Khan',
+    profession: 'Front-end-developer'
+  }
+
+  var myStyle = {
+    color:'#eb1b33'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={myStyle}>{mySelf.name}</h2>
+        <p style={{fontStyle:'italic',color:'#eb1'}}>{mySelf.profession}</p>
       </header>
     </div>
   );
